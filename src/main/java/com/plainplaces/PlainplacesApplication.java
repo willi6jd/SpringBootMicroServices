9fd2be2bacaf9ffbb40c4fa8a04925eda493dd97
Administrator <Administrator@DESKTOP-099UQRG>
2019-09-03 14:56:23 -0400

    Initial commit of Spring Boot Project

---
+++ src/main/java/com/plainplaces/PlainplacesApplication.java
@@ -0,0 +1,13 @@
+package com.plainplaces;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlainplacesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlainplacesApplication.class, args);
+	}
+
+}
